fe3412729803a2f872093444eb3fd84c97bb3172
eduardoqsdev <eduardoqsilva@outlook.com>
2021-09-03 22:23:24 -0300

    Update exercício

--- CursoEmVideo/desafio062.py
+++ CursoEmVideo/desafio062.py
@@ -2,10 +2,9 @@ print('******** Os termos de uma PA ********')
 termo1 = int(input("Digite o primeiro termo: "))
 razao = int(input("Digite a razão: "))
 e = int(input("Digite quantos termos você quer exibir: "))
-valor = 0
 while e != 0:
-    valor += razao
-    print(f"\033[0:33:40m{valor} ->", end=" \033[m")
+    print(f"\033[0:33:40m{termo1} ->", end=" \033[m")
+    termo1 += razao
     e -= 1
     if e == 0:
         e = int(input("\n\033[0:32:40mDigite o número de termos, ou [0] para sair: \033[m"))
