23308991f8a41a4941ee7979d9b957f067227e65
eduardoqsdev <eduardoqsilva@outlook.com>
2021-09-04 22:28:59 -0300

    Exercícios 66, 67, 68.

--- CursoEmVideo/desafio068.py
+++ CursoEmVideo/desafio068.py
@@ -1,26 +1,27 @@
 from random import randint
 num = randnum = c = v = 0
 d = " "
+print("*"*20,"PAR OU ÍMPAR","*"*20)
 while True:
     num = int(input("Digite um número de 1 a 10: "))
-    esclh = int(input("Digite 1 [Par] ou 2 [Ímpar] "))
+    esclh = str(input("Digite [P] para par ou [I] para ímpar: ")).strip().upper()[0]
     c += 1
     randnum = randint(1, 10)
     if (num + randnum) % 2 == 0:
         d = "PAR"
-        if esclh == 1:
+        if esclh == "P":
             print(f"\033[0:32:40m{num + randnum} é par você venceu!\033[m")
             v += 1
-        if esclh == 2:
-            print(f"\033[0:31:40m{num + randnum} é par Você perdeu!\033[m")
+        if esclh == "I":
+            print(f"\033[0:31:40m{num + randnum} é par você perdeu!\033[m")
             break
     if (num + randnum) % 2 != 0:
         d = "ÍMPAR"
-        if esclh == 2:
+        if esclh == "I":
             print(f"\033[0:32:40m{num + randnum} é ímpar você venceu!\033[m")
             v += 1
-        if esclh == 1:
-            print(f"\033[0:31:40m{num + randnum} é impar e você perdeu!\033[m")
+        if esclh == "P":
+            print(f"\033[0:31:40m{num + randnum} é ímpar e você perdeu!\033[m")
             break
 
 print(f"\033[0:31:40mVocê escolheu {num} eo computador escolheu {randnum} com total de {randnum + num} que é {d}, o computador precisou {c} tentativa(s) para vencer!\033[m")
