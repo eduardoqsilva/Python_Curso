ed49207f9aa3c590679b428985c0ae601b208fb0
eduardoqsdev <eduardoqsilva@outlook.com>
2021-10-30 13:54:47 -0300

    update

--- CursoEmVideo/desafio088.py
+++ CursoEmVideo/desafio088.py
@@ -1,20 +1,31 @@
 from random import randint
 from time import sleep
 megaSena = []
+lista = []
 print("=-" * 30)
 print(f"GERADOR DE NÚMEROS MEGA SENA ".center(55))
 print("=-" * 30)
 n = int(input("Quantos jogos? "))
-for c in range(0, n):
-    list = [randint(1, 60), randint(1, 60), randint(1, 60),
-            randint(1, 60), randint(1, 60), randint(1, 60)]
-    megaSena.append(list[:])
-    list.clear()
-print("=-" * 30)
-print(f"SORTEANDO {n} JOGOS!".center(50))
-print("=-" * 30)
-for c in range(0, n):
-    print(f"Jogo {c+ 1}: {megaSena[c]}")
+tot = 1
+while tot <= n:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    megaSena.append(lista[:])
+    lista.clear()
+    tot += 1
+print("=-"*30)
+tot = 1
+for c in megaSena:
+    print(f"Jogo {tot}: {c}")
+    tot += 1
     sleep(1)
-print("=-" * 30)
-print("~~ < BOA SORTE! > ~~".center(50))
+print("=-"*30)
+print("~~ < Boa sorte! > ~~".center(50))
+print("=-"*30)
